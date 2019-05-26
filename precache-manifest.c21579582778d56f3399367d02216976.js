665a8cdd0a600d6fe1ac71fe8d07ba52a7f6544b
dar4kamal <34961537+dar4kamal@users.noreply.github.com>
2019-05-27 01:47:37 +0200

    Updates

---
+++ precache-manifest.c21579582778d56f3399367d02216976.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfc2f30dcc8d74668902",
+    "url": "/Chat-Room-React/static/js/runtime~main.693f8a8b.js"
+  },
+  {
+    "revision": "719ded0dc439bc510127",
+    "url": "/Chat-Room-React/static/js/main.b8c1f57b.chunk.js"
+  },
+  {
+    "revision": "3dce263165337cc08b97",
+    "url": "/Chat-Room-React/static/js/2.9dc78085.chunk.js"
+  },
+  {
+    "revision": "719ded0dc439bc510127",
+    "url": "/Chat-Room-React/static/css/main.2ee22194.chunk.css"
+  },
+  {
+    "revision": "160f52edca30af74cc8e6a4d1ed7b6f7",
+    "url": "/Chat-Room-React/index.html"
+  }
+];
